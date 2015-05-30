65a61136ba72bf8f79c7d092fa43848f48005f0f
Agustina Barbetta <agustina.barbetta@gmail.com>
2015-05-30 15:54:07 -0300

    Metodos de structures agregados

--- src/fiuba/algo3/starcraft/logic/structures/MineralCenter.java
+++ src/fiuba/algo3/starcraft/logic/structures/MineralCenter.java
@@ -1,6 +1,13 @@
 package fiuba.algo3.starcraft.logic.structures;
 
+import fiuba.algo3.starcraft.logic.templates.Life;
+
 public class MineralCenter extends ExploitationStructure {
+
+	MineralCenter(Life life) {
+		super(life);
+		// TODO Auto-generated constructor stub
+	}
 
 	// Es realmente necesaria o tiene el mismo comportamiento que la refineria?
 }
