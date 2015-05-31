c5fa60f362ad73bd403ac0881430b9e43918697c
Agustina Barbetta <agustina.barbetta@gmail.com>
2015-05-31 18:05:38 -0300

    Paquete y primera clase de prueba de Units

---
+++ test/fiuba/algo3/starcraft/test/units/MarineTest.java
@@ -0,0 +1,32 @@
+package fiuba.algo3.starcraft.test.units;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import fiuba.algo3.starcraft.logic.player.Player;
+import fiuba.algo3.starcraft.logic.player.Resources;
+import fiuba.algo3.starcraft.logic.templates.MarineTemplate;
+
+public class MarineTest {
+
+	@Test
+	public void test() {
+		fail("not implemented");
+		/*
+		Resources initialResources = new Resources(200,0);
+		Player player = new Player(initialResources);
+		player.newStructureWithID(StructureID);
+		
+		BarracaTemplate templateBarraca = BarracaTemplate.getInstance();
+		
+		MarineTemplate templateMarine = MarineTemplate.getInstance();
+		
+		MuggleUnit marine = template.create();
+		player.newUnit(unit);
+		
+		assertEquals(player.unitsQuantity(), 1);
+		*/
+	}
+
+}
