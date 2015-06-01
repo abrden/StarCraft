9d988686f1adceec701bc310ab6903c83bfdbbe8
Agustina Barbetta <agustina.barbetta@gmail.com>
2015-05-31 23:11:13 -0300

    Test de Unit actualizado

--- test/fiuba/algo3/starcraft/test/units/MarineTest.java
+++ test/fiuba/algo3/starcraft/test/units/MarineTest.java
@@ -6,27 +6,24 @@ import org.junit.Test;
 
 import fiuba.algo3.starcraft.logic.player.Player;
 import fiuba.algo3.starcraft.logic.player.Resources;
-import fiuba.algo3.starcraft.logic.templates.MarineTemplate;
+import fiuba.algo3.starcraft.logic.structures.ConstructionStructure;
+import fiuba.algo3.starcraft.logic.templates.BarracaTemplate;
+import fiuba.algo3.starcraft.logic.units.Unit;
 
 public class MarineTest {
 
 	@Test
 	public void test() {
-		fail("not implemented");
-		/*
 		Resources initialResources = new Resources(200,0);
 		Player player = new Player(initialResources);
-		player.newStructureWithID(StructureID);
+		player.pays(150, 0);
+		ConstructionStructure barraca = BarracaTemplate.getInstance().create();
+		player.newStructure(barraca);
 		
-		BarracaTemplate templateBarraca = BarracaTemplate.getInstance();
+		Unit marine = barraca.make(1, player.getResources(), player.populationSpace());
+		player.newUnit(marine);
 		
-		MarineTemplate templateMarine = MarineTemplate.getInstance();
-		
-		MuggleUnit marine = template.create();
-		player.newUnit(unit);
-		
-		assertEquals(player.unitsQuantity(), 1);
-		*/
+		assertEquals(player.population(), 1);
 	}
 
 }
