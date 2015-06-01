7ecdf5b84de676c71716edf9c13f601981ae46d2
Agustina Barbetta <agustina.barbetta@gmail.com>
2015-05-31 21:05:17 -0300

    Metodos agregados a ConstructionStructure

--- src/fiuba/algo3/starcraft/logic/structures/ConstructionStructure.java
+++ src/fiuba/algo3/starcraft/logic/structures/ConstructionStructure.java
@@ -2,15 +2,23 @@ package fiuba.algo3.starcraft.logic.structures;
 
 import java.util.Collection;
 
+import fiuba.algo3.starcraft.logic.player.Resources;
 import fiuba.algo3.starcraft.logic.templates.Life;
-import fiuba.algo3.starcraft.logic.templates.Template;
+import fiuba.algo3.starcraft.logic.templates.UnitTemplate;
+import fiuba.algo3.starcraft.logic.units.Unit;
 
 public class ConstructionStructure extends Structure {
 	
-	private Collection<Template> templates;
+	private Collection<UnitTemplate> templates;
 	
-	ConstructionStructure(Life life) {
+	public ConstructionStructure(Life life, Collection<UnitTemplate> templates) {
 		super(life);
+		this.templates = templates;
+	}
+
+	public Unit buildWith(Resources resources) {
+		//TODO: Completar metodo
+		return null;
 	}
 	
 	public StructureID getId() {
