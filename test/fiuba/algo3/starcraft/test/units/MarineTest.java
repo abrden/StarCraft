a0f7723efa2cea5d2eb514e8cb04ff9b635e38a4
Agustina Barbetta <agustina.barbetta@gmail.com>
2015-06-01 02:06:53 -0300

    Pruebas unitarias exitosas para la creacion de Marine

--- test/fiuba/algo3/starcraft/test/units/MarineTest.java
+++ test/fiuba/algo3/starcraft/test/units/MarineTest.java
@@ -18,7 +18,7 @@ import fiuba.algo3.starcraft.logic.units.Unit;
 public class MarineTest {
 
 	@Test
-	public void testMarineCreation() throws InsufficientResources, QuotaExceeded {
+	public void testMarineCreationWith1DepositoSuministro1BarracaAnd50M() throws InsufficientResources, QuotaExceeded {
 		Resources initialResources = new Resources(300,0);
 		Player player = new Player(initialResources);
 		player.pays(100, 0);
@@ -34,4 +34,120 @@ public class MarineTest {
 		assertEquals(player.population(), 1);
 	}
 
+	@Test
+	public void test2MarineCreationWith1DepositoSuministro1BarracaAnd100M() throws InsufficientResources, QuotaExceeded {
+		Resources initialResources = new Resources(400,0);
+		Player player = new Player(initialResources);
+		player.pays(100, 0);
+		Depot depot = DepositoSuministroTemplate.getInstance().create();
+		player.newStructure(depot);
+		player.pays(150, 0);
+		ConstructionStructure barraca = BarracaTemplate.getInstance().create();
+		player.newStructure(barraca);
+		
+		Unit marine1 = barraca.createUnit(TemplateID.MarineTemplate, player.getResources(), player.populationSpace());
+		player.newUnit(marine1);
+		Unit marine2 = barraca.createUnit(TemplateID.MarineTemplate, player.getResources(), player.populationSpace());
+		player.newUnit(marine2);
+		
+		assertEquals(player.population(), 2);
+	}
+	
+	@Test(expected = InsufficientResources.class)
+	public void test2MarineCreationWith1DepositoSuministro1BarracaAnd50M() throws InsufficientResources, QuotaExceeded {
+		Resources initialResources = new Resources(300,0);
+		Player player = new Player(initialResources);
+		player.pays(100, 0);
+		Depot depot = DepositoSuministroTemplate.getInstance().create();
+		player.newStructure(depot);
+		player.pays(150, 0);
+		ConstructionStructure barraca = BarracaTemplate.getInstance().create();
+		player.newStructure(barraca);
+		Unit marine1 = barraca.createUnit(TemplateID.MarineTemplate, player.getResources(), player.populationSpace());
+		player.newUnit(marine1);
+		
+		Unit marine2 = barraca.createUnit(TemplateID.MarineTemplate, player.getResources(), player.populationSpace());
+		player.newUnit(marine2);
+	}
+	
+	@Test(expected = QuotaExceeded.class)
+	public void testMarineCreationWith1BarracaAnd50M() throws InsufficientResources, QuotaExceeded {
+		Resources initialResources = new Resources(200,0);
+		Player player = new Player(initialResources);
+		player.pays(150, 0);
+		ConstructionStructure barraca = BarracaTemplate.getInstance().create();
+		player.newStructure(barraca);
+		
+		Unit marine1 = barraca.createUnit(TemplateID.MarineTemplate, player.getResources(), player.populationSpace());
+		player.newUnit(marine1);
+		
+		assertEquals(player.population(), 2);
+	}
+	
+	@Test
+	public void test2MarineCreationAnd1MarineDeadLeavesPopulationAt1() throws InsufficientResources, QuotaExceeded {
+		Resources initialResources = new Resources(400,0);
+		Player player = new Player(initialResources);
+		player.pays(100, 0);
+		Depot depot = DepositoSuministroTemplate.getInstance().create();
+		player.newStructure(depot);
+		player.pays(150, 0);
+		ConstructionStructure barraca = BarracaTemplate.getInstance().create();
+		player.newStructure(barraca);
+		Unit marine1 = barraca.createUnit(TemplateID.MarineTemplate, player.getResources(), player.populationSpace());
+		player.newUnit(marine1);
+		Unit marine2 = barraca.createUnit(TemplateID.MarineTemplate, player.getResources(), player.populationSpace());
+		player.newUnit(marine2);
+		assertEquals(player.population(), 2);
+		
+		marine2.reduceLife(40);
+		player.newTurn();
+		
+		assertEquals(player.population(), 1);
+	}
+	
+	@Test
+	public void test4MarineCreationAnd4MarineDeadLeavesPopulationAt0() throws InsufficientResources, QuotaExceeded {
+		Resources initialResources = new Resources(100000,0);
+		Player player = new Player(initialResources);
+		player.pays(100, 0);
+		Depot depot = DepositoSuministroTemplate.getInstance().create();
+		player.newStructure(depot);
+		player.pays(150, 0);
+		ConstructionStructure barraca = BarracaTemplate.getInstance().create();
+		player.newStructure(barraca);
+
+		for (int i = 0; i < 4; i++) {
+			Unit marine = barraca.createUnit(TemplateID.MarineTemplate, player.getResources(), player.populationSpace());
+			player.newUnit(marine);
+			
+			assertEquals(player.population(), 1);
+			marine.reduceLife(40);
+			player.newTurn();
+		}
+		
+		assertEquals(player.population(), 0);
+	}
+	
+	@Test
+	public void test7MarineCreationAnd49TurnsLeavesPopulationAt7() throws InsufficientResources, QuotaExceeded {
+		Resources initialResources = new Resources(100000,0);
+		Player player = new Player(initialResources);
+		player.pays(100, 0);
+		Depot depot1 = DepositoSuministroTemplate.getInstance().create();
+		player.newStructure(depot1);
+		player.pays(100, 0);
+		Depot depot2 = DepositoSuministroTemplate.getInstance().create();
+		player.newStructure(depot2);
+		player.pays(150, 0);
+		ConstructionStructure barraca = BarracaTemplate.getInstance().create();
+		player.newStructure(barraca);
+		for (int i = 0; i < 7; i++) {
+			Unit marine = barraca.createUnit(TemplateID.MarineTemplate, player.getResources(), player.populationSpace());
+			player.newUnit(marine);
+			assertEquals(player.population(), i + 1);
+		}
+		
+		assertEquals(player.population(), 7);
+	}
 }
