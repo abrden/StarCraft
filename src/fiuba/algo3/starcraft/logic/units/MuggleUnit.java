bf13804e58de259b32a1e7fce3c38607fcd297ea
santilazzari <santilazzari1994@gmail.com>
2015-06-01 16:27:26 -0300

    Muggle Unit
    Muggle Unit implements can fly method of transportable interface

--- src/fiuba/algo3/starcraft/logic/units/MuggleUnit.java
+++ src/fiuba/algo3/starcraft/logic/units/MuggleUnit.java
@@ -3,18 +3,9 @@ package fiuba.algo3.starcraft.logic.units;
 public class MuggleUnit extends Unit implements Transportable {
 	
 	private Attack attack;
-	private boolean canFly;
-	
-	public void setCanFly(boolean canFly) {
-		this.canFly = canFly;
-	}
 	
 	public int getTransportQuota() {
 		return -1; //TODO: Implementar esta redefinicion de Transportable
-	}
-	
-	public boolean canFly() {
-		return canFly;
 	}
 
 }
