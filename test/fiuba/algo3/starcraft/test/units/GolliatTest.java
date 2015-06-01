a69010404a8a622d205ff7744986d6fb48bf6fbb
Agustina Barbetta <agustina.barbetta@gmail.com>
2015-06-01 11:42:19 -0300

    Pruebas unitarias para Units con una prueba exitosa de creacion cada una

--- test/fiuba/algo3/starcraft/test/units/GolliatTest.java
+++ test/fiuba/algo3/starcraft/test/units/GolliatTest.java
@@ -37,5 +37,30 @@ public class GolliatTest {
 		
 		assertEquals(player.population(), 1);
 	}
-	
+
+	@Test
+	public void test2GolliatCreationAnd1GolliatDeadLeavesPopulationAt1() throws InsufficientResources, QuotaExceeded {
+		Resources initialResources = new Resources(650,200);
+		Player player = new Player(initialResources);
+		player.pays(100, 0);
+		Depot depot = DepositoSuministroTemplate.getInstance().create();
+		player.newStructure(depot);
+		player.pays(150, 0);
+		ConstructionStructure barraca = BarracaTemplate.getInstance().create();
+		player.newStructure(barraca);
+		player.pays(200, 100);
+		ConstructionStructure fabrica = FabricaTemplate.getInstance().create();
+		player.newStructure(fabrica);
+		Unit golliat1 = fabrica.createUnit(TemplateID.GolliatTemplate, player.getResources(), player.populationSpace());
+		player.newUnit(golliat1);
+		Unit golliat2 = fabrica.createUnit(TemplateID.GolliatTemplate, player.getResources(), player.populationSpace());
+		player.newUnit(golliat2);
+		assertEquals(player.population(), 2);
+		
+		golliat2.reduceLife(125);
+		player.newTurn();
+		
+		assertEquals(player.population(), 1);
+	}
 }
+
---
+++ test/fiuba/algo3/starcraft/test/units/ZealotTest.java
@@ -0,0 +1,48 @@
+package fiuba.algo3.starcraft.test.units;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import fiuba.algo3.starcraft.logic.player.Player;
+import fiuba.algo3.starcraft.logic.player.Resources;
+import fiuba.algo3.starcraft.logic.structures.ConstructionStructure;
+import fiuba.algo3.starcraft.logic.structures.Depot;
+import fiuba.algo3.starcraft.logic.structures.InsufficientResources;
+import fiuba.algo3.starcraft.logic.structures.QuotaExceeded;
+import fiuba.algo3.starcraft.logic.templates.AccesoTemplate;
+import fiuba.algo3.starcraft.logic.templates.PilonTemplate;
+import fiuba.algo3.starcraft.logic.templates.TemplateID;
+import fiuba.algo3.starcraft.logic.units.Unit;
+
+public class ZealotTest {
+
+	@Test
+	public void testZealotCreationWith1Pilon1AccesoAnd100M() throws InsufficientResources, QuotaExceeded {
+		Resources initialResources = new Resources(350,0);
+		Player player = new Player(initialResources);
+		player.pays(100, 0);
+		Depot pilon = PilonTemplate.getInstance().create();
+		player.newStructure(pilon);
+		player.pays(150, 0);
+		ConstructionStructure acceso = AccesoTemplate.getInstance().create();
+		player.newStructure(acceso);
+		
+		Unit zealot = acceso.createUnit(TemplateID.ZealotTemplate, player.getResources(), player.populationSpace());
+		player.newUnit(zealot);
+		
+		assertEquals(player.population(), 1);
+	}
+	
+	@Test(expected = QuotaExceeded.class)
+	public void testZealotCreationIsImpossibleWithoutPilon() throws InsufficientResources, QuotaExceeded {
+		Resources initialResources = new Resources(150,0);
+		Player player = new Player(initialResources);
+		player.pays(150, 0);
+		ConstructionStructure acceso = AccesoTemplate.getInstance().create();
+		player.newStructure(acceso);
+		
+		acceso.createUnit(TemplateID.ZealotTemplate, player.getResources(), player.populationSpace());
+	}
+
+}
