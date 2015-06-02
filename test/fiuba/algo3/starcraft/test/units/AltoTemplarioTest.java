8593e22bd82ea19c128a60c0230cf7551111c9c7
Agustina Barbetta <agustina.barbetta@gmail.com>
2015-06-02 12:43:20 -0300

    Test completados y exitosos

--- test/fiuba/algo3/starcraft/test/units/AltoTemplarioTest.java
+++ test/fiuba/algo3/starcraft/test/units/AltoTemplarioTest.java
@@ -4,11 +4,47 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import fiuba.algo3.starcraft.logic.player.Construction;
+import fiuba.algo3.starcraft.logic.player.Player;
+import fiuba.algo3.starcraft.logic.player.Resources;
+import fiuba.algo3.starcraft.logic.structures.ConstructionStructure;
+import fiuba.algo3.starcraft.logic.structures.Depot;
+import fiuba.algo3.starcraft.logic.structures.InsufficientResources;
+import fiuba.algo3.starcraft.logic.structures.QuotaExceeded;
+import fiuba.algo3.starcraft.logic.structures.TemplateNotFound;
+import fiuba.algo3.starcraft.logic.templates.AccesoTemplate;
+import fiuba.algo3.starcraft.logic.templates.ArchivosTemplariosTemplate;
+import fiuba.algo3.starcraft.logic.templates.PilonTemplate;
+import fiuba.algo3.starcraft.logic.templates.PuertoEstelarProtossTemplate;
+import fiuba.algo3.starcraft.logic.units.Unit;
+
 public class AltoTemplarioTest {
 
 	@Test
-	public void test() {
-		fail("Template no implementado yet");
+	public void testAltoTemplarioCreationWith1ArchivosTemplariosAnd50M150G() throws InsufficientResources, QuotaExceeded, TemplateNotFound {
+		Resources initialResources = new Resources(600,500);
+		Player player = new Player(null, null, null, initialResources);
+		player.pays(100, 0);
+		Depot pilon = PilonTemplate.getInstance().create();
+		player.newStructure(pilon);
+		player.pays(150, 0);
+		ConstructionStructure acceso = AccesoTemplate.getInstance().create();
+		player.newStructure(acceso);
+		player.pays(150, 150);
+		ConstructionStructure puerto = PuertoEstelarProtossTemplate.getInstance().create();
+		player.newStructure(puerto);
+		player.pays(150, 200);
+		ConstructionStructure archivos = ArchivosTemplariosTemplate.getInstance().create();
+		player.newStructure(puerto);
+		
+		Construction construction = archivos.create("Alto Templario", player.getResources(), player.populationSpace());
+		while(!construction.itsFinished()) {
+			construction.lowerRelease();
+		}
+		Unit templario = (Unit) construction.gather();
+		player.newUnit(templario);
+		
+		assertEquals(player.currentPopulation(), 2);
 	}
 
 }
--- test/fiuba/algo3/starcraft/test/units/NaveCienciaTest.java
+++ test/fiuba/algo3/starcraft/test/units/NaveCienciaTest.java
@@ -4,11 +4,47 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import fiuba.algo3.starcraft.logic.player.Construction;
+import fiuba.algo3.starcraft.logic.player.Player;
+import fiuba.algo3.starcraft.logic.player.Resources;
+import fiuba.algo3.starcraft.logic.structures.ConstructionStructure;
+import fiuba.algo3.starcraft.logic.structures.Depot;
+import fiuba.algo3.starcraft.logic.structures.InsufficientResources;
+import fiuba.algo3.starcraft.logic.structures.QuotaExceeded;
+import fiuba.algo3.starcraft.logic.structures.TemplateNotFound;
+import fiuba.algo3.starcraft.logic.templates.BarracaTemplate;
+import fiuba.algo3.starcraft.logic.templates.DepositoSuministroTemplate;
+import fiuba.algo3.starcraft.logic.templates.FabricaTemplate;
+import fiuba.algo3.starcraft.logic.templates.PuertoEstelarTerranTemplate;
+import fiuba.algo3.starcraft.logic.units.Unit;
+
 public class NaveCienciaTest {
 
 	@Test
-	public void test() {
-		fail("Template no implementado yet");
+	public void testNeveCienciaCreationWith1PuertoEstelarAnd100M225G() throws QuotaExceeded, InsufficientResources, TemplateNotFound {
+		Resources initialResources = new Resources(700,425);
+		Player player = new Player(null, null, null, initialResources);
+		player.pays(100, 0);
+		Depot depot = DepositoSuministroTemplate.getInstance().create();
+		player.newStructure(depot);
+		player.pays(150, 0);
+		ConstructionStructure barraca = BarracaTemplate.getInstance().create();
+		player.newStructure(barraca);
+		player.pays(200, 100);
+		ConstructionStructure fabrica = FabricaTemplate.getInstance().create();
+		player.newStructure(fabrica);
+		player.pays(150, 100);
+		ConstructionStructure puerto = PuertoEstelarTerranTemplate.getInstance().create();
+		player.newStructure(puerto);
+		
+		Construction construction = puerto.create("Nave Ciencia", player.getResources(), player.populationSpace());
+		while(!construction.itsFinished()) {
+			construction.lowerRelease();
+		}
+		Unit nave = (Unit) construction.gather();
+		player.newUnit(nave);
+		
+		assertEquals(player.currentPopulation(), 2);
 	}
 
 }
