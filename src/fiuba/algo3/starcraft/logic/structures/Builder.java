8ebac038b439faf4bcc48e5a3c024f9a67f909cd
Agustina Barbetta <agustina.barbetta@gmail.com>
2015-06-01 22:49:15 -0300

    Builders y excepcion MissingStructureRequired

--- src/fiuba/algo3/starcraft/logic/structures/Builder.java
+++ src/fiuba/algo3/starcraft/logic/structures/Builder.java
@@ -1,18 +1,34 @@
 package fiuba.algo3.starcraft.logic.structures;
 
 import java.util.Collection;
+import java.util.Map;
 
 import fiuba.algo3.starcraft.logic.player.Resources;
 import fiuba.algo3.starcraft.logic.templates.StructureTemplate;
-import fiuba.algo3.starcraft.logic.templates.TemplateID;
 
 public abstract class Builder {
 
 	protected Collection<StructureTemplate> templates;
+	protected Map<String,String> dependsOn;
 	
-	//Puede recibir la coleccion o quizas recordar las cosas que ya construyo? No? pueden morir todas y ya no ser correcto?
-	public abstract Structure create(TemplateID id, Resources resources, Collection<Structure> built);
+	public Structure create(String name, Resources resources, Collection<Structure> built) throws MissingStructureRequired {
+		if (this.structureRequiredExists(name, built)) {
+			StructureTemplate template = this.getTemplateWithName(name);
+			return template.getInstance().create(); //no le kb 1
+		} else throw new MissingStructureRequired();
+	}
 	
-	//private abstract boolean structureNeededExists(Collection<Structure> built);
+	private StructureTemplate getTemplateWithName(String name) {
+		for (StructureTemplate template : templates) {
+			if (template.getName() == name)
+				return template;
+		}
+		return null;
+	}
+
+	private boolean structureRequiredExists(String name, Collection<Structure> built) {
+		//TODO implementar
+		return false;
+	}
 	
 }
---
+++ src/fiuba/algo3/starcraft/logic/structures/MissingStructureRequired.java
@@ -0,0 +1,7 @@
+package fiuba.algo3.starcraft.logic.structures;
+
+public class MissingStructureRequired extends Exception {
+
+	private static final long serialVersionUID = -9178611779914957174L;
+
+}
