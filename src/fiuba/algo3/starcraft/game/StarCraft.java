0d25e7f33a1a40ac0201ecbf5bfb1838e2ccbaed
Agustina Barbetta <agustina.barbetta@gmail.com>
2015-06-02 02:32:04 -0300

    Ligeros cambios en clase StarCraft

--- src/fiuba/algo3/starcraft/game/StarCraft.java
+++ src/fiuba/algo3/starcraft/game/StarCraft.java
@@ -1,10 +1,20 @@
 package fiuba.algo3.starcraft.game;
 
+import fiuba.algo3.starcraft.logic.map.Map;
+import fiuba.algo3.starcraft.logic.player.Player;
+
 public class StarCraft {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private Map map;
+	private Player player1;
+	private Player player2;
+	
+	public static void start() {
+		// TODO Implementar
+	}
 
+	public static void main(String[] args) {
+		start();
 	}
 
 }
