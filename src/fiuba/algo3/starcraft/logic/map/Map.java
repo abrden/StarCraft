bb9ecd093b19f481e4baee4d5c130b104c919c96
santilazzari <santilazzari1994@gmail.com>
2015-06-01 21:06:45 -0300

    Map
    Map can build a structure in a sepecific point, doesnt raise an exception when the parcel is occupied with other structure

--- src/fiuba/algo3/starcraft/logic/map/Map.java
+++ src/fiuba/algo3/starcraft/logic/map/Map.java
@@ -2,6 +2,8 @@ package fiuba.algo3.starcraft.logic.map;
 
 import java.util.ArrayList;
 
+import fiuba.algo3.starcraft.logic.structures.Structure;
+
 public class Map {
 	final double PARCEL_SIDE = 10;
 
@@ -18,6 +20,7 @@ public class Map {
 			for (int y = 0 ; y < numberOfParcels ; y ++) {
 				System.out.println("estaParcel se hace con X :" + x * PARCEL_SIDE  + " Y : " + y * PARCEL_SIDE + " y lado : " + PARCEL_SIDE);
 				Parcel parcel = new Parcel(new Point(x * PARCEL_SIDE,y * PARCEL_SIDE),PARCEL_SIDE);
+				parcel.setSurface(LandType.land);
 				parcels.add(parcel);
 			}
 		}
@@ -35,5 +38,10 @@ public class Map {
 	
 	public boolean isPointInsideRadiousOfPivotePoint(Point pivotePoint, double radious, Point otherPoint) {
 		return (pivotePoint.distance(otherPoint)) <= radious;
+	}
+	
+	public void buildStructureInPoint(Structure structure, Point point) {
+		Parcel parcel = getParcelContainingPoint(point);
+		parcel.setStructure(structure);
 	}
 }
