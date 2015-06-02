8a2da828ebe6895e56bb12c89f2771670abfd9d6
santilazzari <santilazzari1994@gmail.com>
2015-06-02 02:19:38 -0300

    Map
    Map generate a random distribution of resources, this one can be set inside a rect and can ve pre set with a sepecific dencity of resources

--- src/fiuba/algo3/starcraft/logic/map/Map.java
+++ src/fiuba/algo3/starcraft/logic/map/Map.java
@@ -1,6 +1,7 @@
 package fiuba.algo3.starcraft.logic.map;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import fiuba.algo3.starcraft.logic.structures.Structure;
 
@@ -19,13 +20,24 @@ public class Map {
 	private void createParcels(double numberOfParcels) {
 		for (int x = 0 ; x < numberOfParcels ; x ++) {
 			for (int y = 0 ; y < numberOfParcels ; y ++) {
-				System.out.println("estaParcel se hace con X :" + x * PARCEL_SIDE  + " Y : " + y * PARCEL_SIDE + " y lado : " + PARCEL_SIDE);
 				Parcel parcel = new Parcel(new Point(x * PARCEL_SIDE,y * PARCEL_SIDE),PARCEL_SIDE);
 				parcel.setSurface(LandType.land);
 				parcels.add(parcel);
 			}
 		}
-
+	}
+	
+	public void createRandomResourcesDistributionInRect(Point origin, int side, double density) {
+		Random random = new Random();
+		
+		for (int i = 0; i < density*side ; i++) { 
+			int randomX = (int) (random.nextInt(side) + origin.getX());
+			int randomY = (int) (random.nextInt(side) + origin.getY());
+		
+			boolean isWithVolcano = random.nextBoolean();
+			Parcel parcel = getParcelContainingPoint(new Point(randomX,randomY));
+			parcel.setSurface(isWithVolcano ? ExtractableType.volcano : ExtractableType.reservoir);
+		}
 	}
 	
 	public Parcel getParcelContainingPoint(Point point) throws IndexOutOfBoundsException{
@@ -45,6 +57,4 @@ public class Map {
 		Parcel parcel = getParcelContainingPoint(point);
 		parcel.setStructure(structure);
 	}
-	
-	
 }
