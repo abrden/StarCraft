49488139c05e85092fbf750743cf537805df9e0f
Agustina Barbetta <agustina.barbetta@gmail.com>
2015-06-02 21:10:56 -0300

    Clase Clon creada (hereda de Unit)

---
+++ src/fiuba/algo3/starcraft/logic/units/Clon.java
@@ -0,0 +1,19 @@
+package fiuba.algo3.starcraft.logic.units;
+
+import fiuba.algo3.starcraft.logic.templates.qualities.Life;
+
+public class Clon extends Unit {
+
+	private Unit clonado;
+	private Life life;
+	
+	Clon(Unit clonado, String name) {
+		super(name, new Life(0, clonado.getShield()), clonado.getVision(), 0);
+		this.clonado = clonado;
+	}
+
+	public boolean itsAlive() {
+		return (life.getShield() > 0);
+	}
+
+}
