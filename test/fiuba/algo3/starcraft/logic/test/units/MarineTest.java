5b8387d4c98ec5e5c37b0f00473a0862521cbe65
Agustina Barbetta <agustina.barbetta@gmail.com>
2015-06-06 20:37:56 -0300

    MarineTest a implementar

---
+++ test/fiuba/algo3/starcraft/logic/test/units/MarineTest.java
@@ -0,0 +1,19 @@
+package fiuba.algo3.starcraft.logic.test.units;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import fiuba.algo3.starcraft.logic.units.MuggleUnit;
+
+public class MarineTest {
+
+	@Test
+	public void test() {
+		MuggleUnit marine = new MuggleUnit(null, null, 0, 0, null, 0, 0);
+		
+		
+		
+	}
+
+}
