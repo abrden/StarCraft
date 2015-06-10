f71ca7b103178df2b32a17620ade6daf9e2c1b76
santilazzari <santilazzari1994@gmail.com>
2015-06-10 03:02:27 -0300

    StarCraft
    Test with air and marine passed

--- test/fiuba/algo3/starcraft/game/test/starcraft/StarcraftTest.java
+++ test/fiuba/algo3/starcraft/game/test/starcraft/StarcraftTest.java
@@ -5,8 +5,10 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import fiuba.algo3.starcraft.game.StarCraft;
+import fiuba.algo3.starcraft.logic.map.LandType;
 import fiuba.algo3.starcraft.logic.map.Map;
 import fiuba.algo3.starcraft.logic.map.Point;
+import fiuba.algo3.starcraft.logic.map.ScenarioGenerator;
 import fiuba.algo3.starcraft.logic.templates.units.terran.MarineTemplate;
 import fiuba.algo3.starcraft.logic.units.MuggleUnit;
 import fiuba.algo3.starcraft.logic.units.exceptions.StepsLimitExceeded;
@@ -23,11 +25,23 @@ public class StarcraftTest {
 		MuggleUnit marine = MarineTemplate.getInstance().create(new Point(20,20));
 		
 		game.moveUnitToDestination(marine, new Point(500,32));
-		
-		System.out.println("x : " + marine.getPosition().getX() + " y :" + marine.getPosition().getY());
-		
+				
 		assertTrue(marine.getPosition().getX() > 499 && marine.getPosition().getY() > 31);
+	}
+	
+	@Test
+	public void testStarcraftMovesAMarineInAMapWithGapsAndStopsMovingAfterGap() throws StepsLimitExceeded {
+		Map currentMap = new Map(1000);
+		ScenarioGenerator scenario = new ScenarioGenerator(currentMap);
 		
+		scenario.assignSurfaceDistributionInRect(LandType.air,new Point(50, 50),100, 1);
+		game.setMap(currentMap);
+
+		MuggleUnit marine = MarineTemplate.getInstance().create(new Point(75,0));
+		
+		game.moveUnitToDestination(marine, new Point(75,500));
+				
+		assertTrue(marine.getPosition().getY() < 50);
 	}
 
 }
