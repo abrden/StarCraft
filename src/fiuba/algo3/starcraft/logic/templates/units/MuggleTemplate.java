4f8db9802984a8c8cc106c7cd9aedba8b024e95e
Agustina Barbetta <agustina.barbetta@gmail.com>
2015-06-16 21:07:05 -0300

    Borrado de variables protected viejas

--- src/fiuba/algo3/starcraft/logic/templates/units/MuggleTemplate.java
+++ src/fiuba/algo3/starcraft/logic/templates/units/MuggleTemplate.java
@@ -1,9 +1,6 @@
 package fiuba.algo3.starcraft.logic.templates.units;
 
-import fiuba.algo3.starcraft.logic.templates.qualities.Attack;
 
 public abstract class MuggleTemplate extends UnitTemplate {
-
-	protected Attack attack;
 	
 }
