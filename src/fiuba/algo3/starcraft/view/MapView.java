93414ab30a806eaf781ae1f374072ccb70915cf3
santilazzari <santilazzari1994@gmail.com>
2015-06-20 22:25:31 -0300

    UI
    Map presents in the first third of the full screen, there is a mouse event in each parcel and is easy to handle the click in each parcel itself

--- src/fiuba/algo3/starcraft/view/MapView.java
+++ src/fiuba/algo3/starcraft/view/MapView.java
@@ -1,15 +1,14 @@
 package fiuba.algo3.starcraft.view;
 
 import java.awt.Color;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
-
-import javax.swing.JPanel;
-
+import javax.swing.JComponent;
 import fiuba.algo3.starcraft.logic.map.Map;
 import fiuba.algo3.starcraft.logic.map.Parcel;
 import fiuba.algo3.starcraft.logic.map.Point;
 
-public class MapView extends JPanel {
+public class MapView extends JComponent{
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -17,12 +16,18 @@ public class MapView extends JPanel {
 	
 	public MapView(Map map) {
 		this.map = map;
-//		generatParcelViews(map.getParcelsContainedInARect(new Point(0,0),map.getSide()));
+		setBounds(0, 0, (int)map.getSide(), (int)map.getSide());
+		generatParcelViews(map.getParcelsContainedInARect(new Point(0,0),map.getSide()));
 		setBackground(new Color(0,255,0));
 	}
 	
 	//TODO implement method
 	private void generatParcelViews(ArrayList<Parcel> parcels) {
+		for (Parcel parcel : parcels) {
+			ParcelView parcelView = new ParcelView(parcel);
+			addMouseListener(parcelView);
+			add(parcelView);
+		}
 		
 	}
 }
