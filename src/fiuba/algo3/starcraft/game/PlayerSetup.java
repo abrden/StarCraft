1211dc894c6cb230f407d2fe26795310c3b41133
Agustina Barbetta <agustina.barbetta@gmail.com>
2015-06-21 23:48:23 -0300

    Cambios en StarCraft y avance en StartupView

--- src/fiuba/algo3/starcraft/game/PlayerSetup.java
+++ src/fiuba/algo3/starcraft/game/PlayerSetup.java
@@ -2,18 +2,47 @@ package fiuba.algo3.starcraft.game;
 
 import java.awt.Color;
 
-import fiuba.algo3.starcraft.logic.structures.builders.Builder;
-import fiuba.algo3.starcraft.logic.structures.builders.ProtossBuilder;
-import fiuba.algo3.starcraft.logic.structures.builders.TerranBuilder;
-
 public class PlayerSetup {
 
 	private String name;
 	private Color color;
-	private Builder builder;
+	private RaceType race;
 
-	public Builder getBuilder() {
-		return builder;
+	public PlayerSetup(String name, String color, String race) throws NameIsTooShort {
+		if (name.length() < 4) throw new NameIsTooShort();
+		this.name = name;
+		this.parseColor(color);
+		this.parseRace(race);
+	}
+
+	private void parseColor(String color) {
+		switch (color) {
+        case "Blue":
+        	this.color = Color.BLUE;
+            break;
+        case "Red":
+        	this.color = Color.RED;
+            break;
+        case "Yellow": 
+        	this.color = Color.YELLOW;
+        	break;
+        case "Green": 
+        	this.color = Color.GREEN;
+        	break;
+        }
+	}
+	
+	private void parseRace(String race) {
+		switch (race) {
+        case "Terran": this.race = RaceType.terran;
+                 break;
+        case "Protoss": this.race = RaceType.terran;
+                 break;
+        }
+	}
+	
+	public RaceType getRace() {
+		return race;
 	}
 
 	public Color getColor() {
@@ -24,17 +53,4 @@ public class PlayerSetup {
 		return name;
 	}
 
-	public void setName(String name) throws NameIsTooShort {
-		if (name.length() < 4) throw new NameIsTooShort();
-		else this.name = name;
-	}
-	
-	public void setColor(Color color) {
-		this.color = color;
-	}
-	
-	public void setBuilder(RaceType race) {
-		if (race == RaceType.protoss) this.builder = new ProtossBuilder();
-		if (race == RaceType.terran) this.builder = new TerranBuilder();
-	}
 }
--- src/fiuba/algo3/starcraft/game/StarCraft.java
+++ src/fiuba/algo3/starcraft/game/StarCraft.java
@@ -6,17 +6,21 @@ import java.util.List;
 import fiuba.algo3.starcraft.logic.map.Map;
 import fiuba.algo3.starcraft.logic.map.Point;
 import fiuba.algo3.starcraft.logic.map.ScenarioGenerator;
+import fiuba.algo3.starcraft.logic.map.areas.LandType;
 import fiuba.algo3.starcraft.logic.map.resources.ReservoirType;
 import fiuba.algo3.starcraft.logic.player.Player;
 import fiuba.algo3.starcraft.logic.player.Resources;
+import fiuba.algo3.starcraft.logic.structures.builders.Builder;
+import fiuba.algo3.starcraft.logic.structures.builders.ProtossBuilder;
+import fiuba.algo3.starcraft.logic.structures.builders.TerranBuilder;
 import fiuba.algo3.starcraft.logic.units.Unit;
-import fiuba.algo3.starcraft.view.StartupView;
 
 public class StarCraft {
 	
 	//Eliminar luego de implementar mejora
 	private Player player1;
 	private Player player2; 
+	public StarCraft() {}
 	public void setGame(Player player1, Player player2, Map map) {
 		this.player1 = player1;
 		this.player2 = player2;
@@ -27,48 +31,28 @@ public class StarCraft {
 	private List<Player> players = new ArrayList<Player>();
 	
 	private ScenarioGenerator scenarioGenerator = new ScenarioGenerator(map);
-	private StartupView startupView;
 	private static final int INITIAL_MINERAL = 200;
 	private static final int INITIAL_GAS = 0;
 	private static final int MAP_SIDE = 1000;
 	private static final int BASE_SIDE = 30;
 	private static final double RESERVOIR_DENSITY = 0.2;
+	private static final double AIR_DENSITY = 0.4;
 	
-	
-	public void gameSetup() {
+	public StarCraft(List<PlayerSetup> playerSetups) {
 		this.generateMap();
-		int quantity = startupView.playersQuantity();
+		
+		int quantity = playerSetups.size();
 		List<Point> bases = this.generateBases(quantity);
 		
-		for (int i = 1; i <= quantity; i++) {
-			int playerNumber = i;
-			PlayerSetup setup = startupView.playerSetup(playerNumber);
-			while (true) {
-				try {
-					checkPlayerSetup(setup);
-					break;
-				} catch (NameIsTaken | ColorIsTaken e) {
-					setup = startupView.newPlayerSetupDueToError(e.getMessage(), playerNumber);
-				}
-			}
-			this.generatePlayer(setup, bases.get(playerNumber));
+		int i = 0;
+		for(PlayerSetup setup : playerSetups) {
+			this.generatePlayer(setup, bases.get(i));
+			i++;
 		}
 	}
 	
-	private boolean checkPlayerSetup(PlayerSetup setup) throws NameIsTaken, ColorIsTaken {
-		for (Player player : players) {
-			if (player.getName() == setup.getName())
-				throw new NameIsTaken();
-			if (player.getColor() == setup.getColor())
-				throw new ColorIsTaken();
-		} 
-		return true;
-	}
-	
 	private void generateMap() {
-		//scenarioGenerator.assignSurfaceDistributionInRect(LandType.land, origin, side, density);
-		//scenarioGenerator.assignSurfaceDistributionInRect(LandType.air, origin, side, density);
-		//el mapa se inicia con todo tierra, todo aire???
+		scenarioGenerator.assignSurfaceDistributionInRect(LandType.air, new Point(MAP_SIDE/2, MAP_SIDE/2), MAP_SIDE, AIR_DENSITY);
 	}
 	
 	private List<Point> generateBases(int quantity) {
@@ -83,7 +67,9 @@ public class StarCraft {
 	}
 	
 	private void generatePlayer(PlayerSetup setup, Point base) {
-		players.add(new Player(setup.getName(), setup.getColor(), setup.getBuilder(), base, new Resources(INITIAL_MINERAL, INITIAL_GAS), map));
+		Builder builder = (setup.getRace() == RaceType.terran ? new TerranBuilder() : new ProtossBuilder());
+		
+		players.add(new Player(setup.getName(), setup.getColor(), builder, base, new Resources(INITIAL_MINERAL, INITIAL_GAS), map));
 	}
 
 	public Iterable<Unit> getEnemyUnits(Iterable<Unit> playerUnits) {
