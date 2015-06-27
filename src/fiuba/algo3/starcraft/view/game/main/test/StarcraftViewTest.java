26b794f83084835c4a084185148e0c3ab640f84c
santilazzari <santilazzari1994@gmail.com>
2015-06-27 19:55:09 -0300

    New path to main

---
+++ src/fiuba/algo3/starcraft/view/game/main/test/StarcraftViewTest.java
@@ -0,0 +1,37 @@
+package fiuba.algo3.starcraft.view.game.main.test;
+
+import java.util.ArrayList;
+
+import fiuba.algo3.starcraft.game.PlayerSetup;
+import fiuba.algo3.starcraft.game.StarCraft;
+import fiuba.algo3.starcraft.view.StarcraftView;
+import fiuba.algo3.starcraft.view.exceptions.NameIsTooShort;
+
+
+public class StarcraftViewTest {
+
+	static StarCraft game;
+	public static void initVariables() {
+		ArrayList<PlayerSetup> playerSetups = new ArrayList<PlayerSetup>();
+			
+		try {
+			playerSetups.add(new PlayerSetup("santi", "red", "Terran"));
+			playerSetups.add(new PlayerSetup("agus", "blue", "Terran"));
+		} catch (NameIsTooShort e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+		game = new StarCraft(playerSetups);		
+	}
+	
+	public static void main(String[] args) {
+		initVariables();
+		initGame();
+}
+	public static void initGame() {		
+		StarcraftView starcraftView = new StarcraftView(game);
+		starcraftView.setVisible(true);
+	}
+
+}
