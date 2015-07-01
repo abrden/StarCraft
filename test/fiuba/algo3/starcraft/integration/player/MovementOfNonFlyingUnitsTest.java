2ce30d6cb207bdaca05a52fe2d54a519299d698a
Agustina Barbetta <agustina.barbetta@gmail.com>
2015-06-30 21:17:28 -0300

    Prueba arreglada

--- test/fiuba/algo3/starcraft/integration/player/MovementOfNonFlyingUnitsTest.java
+++ test/fiuba/algo3/starcraft/integration/player/MovementOfNonFlyingUnitsTest.java
@@ -18,7 +18,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 
 public class MovementOfNonFlyingUnitsTest {
